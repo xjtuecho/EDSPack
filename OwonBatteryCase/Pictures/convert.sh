4789060e634230f866902d157b978db4fde1f4dd
Echo <echo.xjtu@gmail.com>
2020-10-15 20:19:50 +0800

    add Solidworks case from Matthew Pearce <mpea413@aucklanduni.az.nz>

---
+++ OwonBatteryCase/Pictures/convert.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Purpose: batch image resizer
+# Source: https://guides.wp-bullet.com
+# Author: Mike
+
+# absolute path to image folder
+FOLDER="./"
+
+# max width
+WIDTH=2080
+# max height
+HEIGHT=1170
+
+#resize png or jpg to either height or width, keeps proportions using imagemagick
+#find ${FOLDER} -iname '*.jpg' -o -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+#resize png to either height or width, keeps proportions using imagemagick
+#find ${FOLDER} -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+#resize jpg only to either height or width, keeps proportions using imagemagick
+find ${FOLDER} -iname '*.jpg' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+# alternative
+#mogrify -path ${FOLDER} -resize ${WIDTH}x${HEIGHT}% *.png -verbose
